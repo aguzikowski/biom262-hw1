353ea131bf3a00efaa9e462fb78fecdf2f9ec0d6
afsheenbanisadr <afsheen.banisadr@gmail.com>
2016-01-18 16:14:35 -0800

    Made the LICENSE file and added excercise 5 to the tf_binding file

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,2 +1,15 @@
-Test
 
+## Excercise 5
+%%bash --out exercise5
+
+module load biotools
+
+bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -s -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+
+wc -l gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+echo '--- First 10 lines ---'
+head gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+echo '--- Random 10 lines ---'
+awk -v seed=908 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta | head
+echo '--- Last 10 lines ---'
+tail gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
