ea6aef115df43d682f46c97dd371f02016cf38bc
afsheenbanisadr <afsheen.banisadr@gmail.com>
2016-01-18 16:19:31 -0800

    Added excercise 4 to the tf_binding file

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,3 +1,17 @@
+## Exercise 4
+%%bash --out exercise4
+
+module load biotools
+
+bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+
+wc -l gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+echo '--- First 10 lines ---'
+head gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+echo '--- Random 10 lines ---'
+awk -v seed=908 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf | head
+echo '--- Last 10 lines ---'
+tail gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
 
 ## Excercise 5
 %%bash --out exercise5
