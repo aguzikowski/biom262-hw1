3df4b79f863b89fc65dd17cc7b129349f92ba724
afsheenbanisadr <afsheen.banisadr@gmail.com>
2016-01-21 13:07:49 -0800

    Finally fixed all of the errors

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,4 +1,4 @@
-#!/bin/csh
+#!/bin/bash
 #PBS -q hotel
 #PBS -N tf_binding.sh
 #PBS -l nodes=1:ppn=1
@@ -8,7 +8,7 @@
 #PBS -V
 #PBS -M arguziko@ucsd.edu,abanisad@ucsd.edu
 #PBS -m abe
-cd /home/ucsd-train15/code/biom262-2016/weeks/week01/data/
+cd /home/ucsd-train15/code/biom262-hw1/data/
 
 echo "Hello I am a message in standard out (stdout)"
 
@@ -17,27 +17,13 @@ echo "Hello I am a message in standard error (stderr)" >&2
 
 ## Exercise 1
 
-cat tf.bed | awk -F "\t" '{if ($4 == "NFKB") {print $0}}' tf.bed  > tf.nfkb.bed
+awk -F "\t" '{if ($4 == "NFKB") {print $0}}' tf.bed  > tf.nfkb.bed
 
-wc -l tf.nfkb.bed
-echo '--- First 10 lines ---'
-head tf.nfkb.bed
-echo '--- Random 10 lines ---'
-awk -v seed=907 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}'  tf.nfkb.bed | head
-echo '--- Last 10 lines ---'
-tail tf.nfkb.bed
 
 ## Exercise 2
 
-cat gencode.v19.annotation.chr22.gtf | awk -F "\t" '{if($3 == "transcript"){print $0}}' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.chr22.transcript.gtf
+awk -F "\t" '{if($3 == "transcript"){print $0}}' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.chr22.transcript.gtf
 
-wc -l gencode.v19.annotation.chr22.transcript.gtf
-echo '--- First 10 lines ---'
-head gencode.v19.annotation.chr22.transcript.gtf
-echo '--- Random 10 lines ---'
-awk -v seed=907 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}'  gencode.v19.annotation.chr22.transcript.gtf | head
-echo '--- Last 10 lines ---'
-tail gencode.v19.annotation.chr22.transcript.gtf
 
 ## Exercise 3
 
@@ -45,25 +31,11 @@ module load biotools
 
 bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
 
-wc -l gencode.v19.annotation.chr22.transcript.promoter.gtf
-echo '--- First 10 lines ---'
-head gencode.v19.annotation.chr22.transcript.promoter.gtf
-echo '--- Random 10 lines ---'
-awk -v seed=907 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.gtf | head
-echo '--- Last 10 lines ---'
-tail gencode.v19.annotation.chr22.transcript.promoter.gtf
 
 ## Exercise 4
 
 bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
 
-wc -l gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
-echo '--- First 10 lines ---'
-head gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
-echo '--- Random 10 lines ---'
-awk -v seed=908 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf | head
-echo '--- Last 10 lines ---'
-tail gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
 
 ## Excercise 5
 
@@ -71,10 +43,4 @@ module load biotools
 
 bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -s -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
 
-wc -l gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
-echo '--- First 10 lines ---'
-head gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
-echo '--- Random 10 lines ---'
-awk -v seed=908 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta | head
-echo '--- Last 10 lines ---'
-tail gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+echo $PWD
